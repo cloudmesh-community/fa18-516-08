c5cb4019650085f23de1bb25c4b0224bb280f813
varunjoshi01 <36316197+varunjoshi01@users.noreply.github.com>
2018-11-29 20:26:34 -0600

    updated files

--- project-code/shell/setup.sh
+++ project-code/shell/setup.sh
@@ -8,7 +8,7 @@ git clone https://github.com/cloudmesh-community/fa18-516-08
 
 cd /opt/cloudmesh/fa18-516-08/project-code
 ##### install requirements.txt #####
-sudo pip3 install -r requirements.txt
+sudo pip install -r requirements.txt
 
 
 
